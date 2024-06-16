090e9f27a10787bab5f3532fdb2ec2570a03d52f
jameskiprop <jameskiprop01@gmail.com>
2024-06-16 23:05:06 +0300

    added a html, and teo js files to the repository

---
+++ arraymethods.js
@@ -0,0 +1,40 @@
+/*switch*/
+var grade = "F";
+
+switch (grade) {
+  case "A":
+    console.log("You did great!");
+    break;
+
+  case "B":
+    console.log("You did Good!");
+    break;
+
+  case "C":
+    console.log("You did Okay!");
+    break;
+
+  case "D":
+    console.log("You didn't do well!");
+    break;
+
+  case "F":
+    console.log("You FAILED!");
+    break;
+
+  default:
+    console.log("grade is not a letter Grade");
+    break;
+}
+
+/*if (grade == "A") {
+  console.log("You did great!");
+} else if (grade == "B") {
+  console.log("you did good");
+} else if (grade == "C") {
+  console.log("you did okay");
+} else if (grade == "D") {
+  console.log("you did not do that well");
+} else if (grade == "F") {
+  console.log("you FAILED!");
+} */
