56eb5ebc4064fb71f3bd3dbed6c1f98aadce5de6
jameskiprop <jameskiprop01@gmail.com>
2024-06-19 16:57:36 +0300

    assignment

--- arraymethods.js
+++ arraymethods.js
@@ -1,118 +1,138 @@
-// 1; //toString()
-/*let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
+// // 1; //toString()
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
 
-let stringNames = names.toString();
-console.log(stringNames);*/
+// let stringNames = names.toString();
+// console.log(stringNames);
 
-// 2; //join
+// // 2; //join
 
-/*let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
 
-let stringNames = names.join(" and ");
-console.log(stringNames); */
+// let stringNames = names.join(" and ");
+// console.log(stringNames);
 
-// 3; //concat -combine arrays together
+// // 3; //concat -combine arrays together
 
-/* let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
-let names2 = ["John", "Tom"];
-let names3 = ["Delilah", "Smith"];
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
+// let names2 = ["John", "Tom"];
+// let names3 = ["Delilah", "Smith"];
 
-let joined = names.concat(names2, names3);
-console.log(joined); */
+// let joined = names.concat(names2, names3);
+// console.log(joined);
 
-// 4; //splice - removing some elements from the array and you can also add new elements
+// // 4; //splice - removing some elements from the array and you can also add new elements
 
-/*let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
-let namesCopy = [...names];
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
+// let namesCopy = [...names];
 
-namesCopy.splice(1, 2, "wanjiru", "Akinyi");
-console.log(namesCopy);*/
+// namesCopy.splice(1, 2, "wanjiru", "Akinyi");
+// console.log(namesCopy);
 
-// 5; //slice - to slice out elements of an array
+// // 5; //slice - to slice out elements of an array
 
-/*let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku"];
 
-let slice = names.slice(1, 3);
-console.log(slice);
-console.log(names);*/
+// let slice = names.slice(1, 3);
+// console.log(slice);
+// console.log(names);
 
-// 6; // index of
+// // 6; // index of - finds the position of an individual element in the array
 
-/*let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku", "Tracy"];
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku", "Tracy"];
 
-let index = names.indexOf("Tracy");
-console.log(index);
-*/
+// let index = names.indexOf("Tracy");
+// console.log(index);
 
-// 7; //lastIndexOf
+// // 7; //lastIndexOf
 
-/*let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku", "Tracy"];
+// let names = ["Danny", "Joe", "Tracy", "Wendy", "Wanjiku", "Tracy"];
 
-let index = names.lastIndexOf("Tracy");
-console.log(index); */
+// let index = names.lastIndexOf("Tracy");
+// console.log(index);
 
-// 8; //flat
-/*let nums = [1, 2, 3, 4, [5, 6, [7, 8]], 9];
+// // 8; //flat
+// let nums = [1, 2, 3, 4, [5, 6, [7, 8]], 9];
 
-let flat = nums.flat();
-console.log(flat);*/
+// let flat = nums.flat();
+// console.log(flat);
 
-// 9; //forEach
-/*let names = ["Danny", "Joe", "Tracy1", "Wendy", "Wanjiku", "Tracy"];
+// // 9; //forEach -Executes a provided function once for each array element without returning a value.
+// let names = ["Danny", "Joe", "Tracy1", "Wendy", "Wanjiku", "Tracy"];
 
-for (let i = 1; i < names.length; i += 2) {
-  console.log(names[i]);
-} */
+// for (let i = 1; i < names.length; i += 2) {
+//   console.log(names[i]);
+// }
 
-//10 map
+// //10 map - Makes a new array by changing each item in an array.
 
-/*let nums = [1, 2, 3, 4, 5, 6];
+// let nums = [1, 2, 3, 4, 5, 6];
 
-let quadrupled = nums.map((n) => n * 4);
-console.log(quadrupled);*/
+// let quadrupled = nums.map((n) => n * 4);
+// console.log(quadrupled);
 
-// 11 Filter
-/*let posts = [
-  { title: "post 1", author: "Jamie" },
-  { title: "post 2", author: "Quincy" },
-  { title: "post 3", author: "Sasha" },
-  { title: "post 4", author: "Troy" },
-];
+// // 11 Filter - Makes a new array with only the items that pass a test.
+// let posts = [
+//   { title: "post 1", author: "Jamie" },
+//   { title: "post 2", author: "Quincy" },
+//   { title: "post 3", author: "Sasha" },
+//   { title: "post 4", author: "Troy" },
+// ];
 
-let QuincysPost = posts.filter((p) => p.author === "Sarah");
-console.log(QuincysPost);*/
+// let QuincysPost = posts.filter((p) => p.author === "Sarah");
+// console.log(QuincysPost);
 
-//12 reduce
-/*let nums = [1, 2, 3, 4, 5, 6, 7];
+// //12 reduce - Combines all items in an array into a single value.
+// let nums = [1, 2, 3, 4, 5, 6, 7];
 
-let total = nums.reduce((total, currentVal) => total + currentVal);
-let max = nums.reduce((a, b) => Math.max(a, b), -Infinity);
-console.log(max);*/
+// let total = nums.reduce((total, currentVal) => total + currentVal);
+// let max = nums.reduce((a, b) => Math.max(a, b), -Infinity);
+// console.log(max);
 
-//13.some
-/*let nums = [1, 2, 3, 4, 5, 6, 7];
+// //13.some - Checks if at least one item in an array passes a test.
+// let nums = [1, 2, 3, 4, 5, 6, 7];
 
-let isGreaterThan5 = nums.some((n) => n > 7);
-console.log(isGreaterThan5);*/
+// let isGreaterThan5 = nums.some((n) => n > 7);
+// console.log(isGreaterThan5);
 
-//14 .every()
-let nums = [1, 2, 3, 0, 5, 6, 7];
+// //14 .every() - Checks if all items in an array pass a test.
+// let nums = [1, 2, 3, 0, 5, 6, 7];
 
-let allGreaterThan0 = nums.every((n) => n > 0);
+// let allGreaterThan0 = nums.every((n) => n > 0);
 
-console.log(allGreaterThan0);
-//15 find
-let stock = [
-  { item: "ketchup", quantity: 32 },
-  { item: "mayo", quantity: 9 },
-  { item: "not sauce", quantity: 12 },
-];
+// console.log(allGreaterThan0);
 
-let ketchup = stock.find((s) => s.item === "ketchup");
-console.log(ketchup.quantity);
+// //15 find - Finds the first item in an array that passes a test.
+// let stock = [
+//   { item: "ketchup", quantity: 32 },
+//   { item: "mayo", quantity: 9 },
+//   { item: "not sauce", quantity: 12 },
+// ];
 
-//16 Sort
-let names = ["Sasha", "Denis", "Troy", "Sarah"];
+// let ketchup = stock.find((s) => s.item === "ketchup");
+// console.log(ketchup.quantity);
 
-let alph = names.sort();
-console.log(alph);
+// //16 Sort Arranges items in an array in order.
+// let names = ["Sasha", "Denis", "Troy", "Sarah"];
+
+// let alph = names.sort();
+// console.log(alph);
+
+const student = {
+  firstName: "John",
+  lastname: "Quincy",
+  age: 24,
+};
+
+function studentGeneration(student) {
+  if (student.age <= 13) {
+    console.log("Gen Alpha");
+  } else if (student.age <= 27) {
+    console.log("gen z");
+  } else if (student.age >= 28) {
+    console.log("Millenial");
+  } else student.age >= 38;
+  {
+    console.log("Boomer");
+  }
+}
+studentGeneration(student);
--- stringmethods.js
+++ stringmethods.js
@@ -1,64 +1,64 @@
-/*20 string methods*/
-var stringOne = "freecodecamp is the best place to learn";
-var stringTwo = "frontend and backend development";
+// /*20 string methods*/
+// var stringOne = "freecodecamp is the best place to learn";
+// var stringTwo = "frontend and backend development";
 
-//charAt()
-console.log(stringOne.charAt(1));
+// //charAt()
+// console.log(stringOne.charAt(1));
 
-//charCodeAt()
-console.log(stringOne.charCodeAt(1));
+// //charCodeAt()
+// console.log(stringOne.charCodeAt(1));
 
-//concat()
-console.log(stringOne.concat(stringTwo));
+// //concat()
+// console.log(stringOne.concat(stringTwo));
 
-//endswith - checks whether a string ends with a specified strig or character
-console.log(stringOne.endsWith("learn"));
+// //endswith - checks whether a string ends with a specified strig or character
+// console.log(stringOne.endsWith("learn"));
 
-//fromCharCode
-console.log(String.fromCharCode(114));
+// //fromCharCode
+// console.log(String.fromCharCode(114));
 
-//includes
-console.log(stringTwo.includes("end"));
+// //includes
+// console.log(stringTwo.includes("end"));
 
-//indexOf() -returns the position of the first found occurrence of a specified value in a string
-console.log(stringTwo.indexOf("end"));
+// //indexOf() -returns the position of the first found occurrence of a specified value in a string
+// console.log(stringTwo.indexOf("end"));
 
-//lastIndexOf() - returns the position of the last found occurrence of a specified value in a string
-console.log(stringTwo.lastIndexOf("end"));
+// //lastIndexOf() - returns the position of the last found occurrence of a specified value in a string
+// console.log(stringTwo.lastIndexOf("end"));
 
-//match - searches a string for a match against a certain expression
-console.log(stringTwo.match(/end/g));
+// //match - searches a string for a match against a certain expression
+// console.log(stringTwo.match(/end/g));
 
-//repeat - returns a new string with a specified number of copies of an existing string
-console.log(stringOne.repeat(3));
+// //repeat - returns a new string with a specified number of copies of an existing string
+// console.log(stringOne.repeat(3));
 
-//replace -searches a string for a specified value /expression and returns a new string where the specified values are
-console.log(stringTwo.replace(/end/g, "END"));
+// //replace -searches a string for a specified value /expression and returns a new string where the specified values are
+// console.log(stringTwo.replace(/end/g, "END"));
 
-//search
-console.log(stringTwo.search("end"));
+// //search
+// console.log(stringTwo.search("end"));
 
-//slice() - extracts a part of a string and returns a new string
-console.log(stringTwo.slice(2, 4));
+// //slice() - extracts a part of a string and returns a new string
+// console.log(stringTwo.slice(2, 4));
 
-//split()
-console.log(stringOne.split(" "));
+// //split()
+// console.log(stringOne.split(" "));
 
-//startsWith()
-console.log(stringOne.startsWith("free"));
+// //startsWith()
+// console.log(stringOne.startsWith("free"));
 
-//substr() - extracts a character from a string beginnign from a specified string
-console.log(stringTwo.substr(2, 4));
+// //substr() - extracts a character from a string beginnign from a specified string
+// console.log(stringTwo.substr(2, 4));
 
-//substring - extracts the characters of a string between two specified indexes
-console.log(stringTwo.substring(2, 4));
+// //substring - extracts the characters of a string between two specified indexes
+// console.log(stringTwo.substring(2, 4));
 
-//toLowerCase
-console.log(stringOne.toLowerCase());
+// //toLowerCase
+// console.log(stringOne.toLowerCase());
 
-//toUpperCase
-console.log(stringOne.toUpperCase());
+// //toUpperCase
+// console.log(stringOne.toUpperCase());
 
-//trim() - removes extra space from a string
-var stringThree = "        Subscribe Now!";
-console.log(stringThree.trim());
+// //trim() - removes extra space from a string
+// var stringThree ="        Subscribe Now!"
+// console.log(stringThree.trim());
